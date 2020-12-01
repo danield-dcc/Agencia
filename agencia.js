eb726316ffe3bdb0d878480418471ccac813b95b
Daniel Conceição <danieldcc.colares@gmail.com>
2020-12-01 15:15:19 -0300

    :sparkles: Adiciona funcao selecionar participante Adiciona um novo botão e a função de selecionar o participante que vai ser excluido

--- agencia.js
+++ agencia.js
@@ -68,7 +68,6 @@ const cadastarPessoa = () => {
         }
     }
 
-
     //verificar se há localStorage
     if (localStorage.getItem("pessoasCadastradas")) {
         //se houver conteudo
@@ -77,7 +76,6 @@ const cadastarPessoa = () => {
         let pessoasCadastradas = localStorage.getItem("pessoasCadastradas") + ";" + nome;
         let emailCadastrado = localStorage.getItem("emailCadastrado") + ";" + email;
         let destinoCadastrado = localStorage.getItem("destinoCadastrado") + ";" + destino;
-
 
         //salvar
         localStorage.setItem("pessoasCadastradas", pessoasCadastradas);
@@ -100,77 +98,120 @@ btCadastrar.addEventListener('click', cadastarPessoa);
 
 
 const mostrarLista = () => {
-    let outCadastros = document.querySelector("#outCadastros");
-    //verificar se a lista não esta vazia
 
-    if (!localStorage.getItem("pessoasCadastradas")) {
-        outCadastros.textContent = "";
-        return;
+    if (localStorage.getItem("pessoasCadastradas")) {
+        //criar um vetor com a list de pessoas 
+        let conteudoNomes = localStorage.getItem("pessoasCadastradas").split(";");
+        let conteudoEmail = localStorage.getItem("emailCadastrado").split(";");
+        let conteudoDestino = localStorage.getItem("destinoCadastrado").split(";");
+
+        //criar referência ao divQuadro(local onde as tags h5 serão inseridas)
+        let divQuadro = document.getElementById("divQuadro")
+
+        for (let i = 0; i < conteudoNomes.length; i++) {
+            let h5 = document.createElement("h5");
+
+            let participantes = document.createTextNode(conteudoNomes[i]);
+            let email = document.createTextNode(conteudoEmail[i]);
+            let destino = document.createTextNode(conteudoDestino[i]);
+            let h5Nome = document.createTextNode("Nome: ")
+            let h5email = document.createTextNode(" - Email: ")
+            let h5destino = document.createTextNode(" - Destino: ")
+            h5.appendChild(h5Nome)
+            h5.appendChild(participantes)   //participantes é filho de h5
+            h5.appendChild(h5email)
+            h5.appendChild(email)   //participantes é filho de h5
+            h5.appendChild(h5destino)
+            h5.appendChild(destino)   //participantes é filho de h5
+            divQuadro.appendChild(h5);      //h5 filho de divQuadro
+        }
     }
-
-    let conteudoNomes = localStorage.getItem("pessoasCadastradas").split(";");
-    let conteudoEmail = localStorage.getItem("emailCadastrado").split(";");
-    let conteudoDestino = localStorage.getItem("destinoCadastrado").split(";");
-
-    let linhas = "";
-
-    for (let i = 0; i < conteudoNomes.length; i++) {
-        linhas += "Nome: " + conteudoNomes[i] + " -" + " Email:" + conteudoEmail[i] + " -" + " Destino:" + conteudoDestino[i] + "\n"
-    }
-
-    outCadastros.textContent = linhas;
-
-
 
 
 }
 mostrarLista();
 
+const selecionar = () => {
+
+    let h5 = document.getElementsByTagName("h5");
+    let numh5 = h5.length;
+
+    if (numh5 == 0) {
+        alert("Não há participantes para selecionar");
+        return
+    }
+
+    let aux = -1; //variavel auxiliar para indicar linha selecionada
+    //percorrendo a lista de elementos h5 inseridos na pagina
+    for (let i = 0; i < numh5; i++) {
+        //se a tag é da class ParticipanteSelecionado (indica que esta selecionada)
+        if (h5[i].className == "ParticipanteSelecionado") {
+            h5[i].className = "Normal"  //troca para normal
+            aux = i;                    //muda valro da aux
+            break                       //sai da repetição
+        }
+    }
+    //se a linha selecionada for a última, voltar para o ínicio
+    if (aux == numh5 - 1) {
+        aux = -1;
+    }
+
+    h5[aux + 1].className = "ParticipanteSelecionado"; //mudar o estilo da pxima tag
+
+}
+let btSelecionar = document.querySelector('#btSelecionar');
+btSelecionar.addEventListener("click", selecionar);
 
 
 const excluir = () => {
-    if (!localStorage.getItem("pessoasCadastradas")) {
-        alert('Não há Pessoas Cadastradas!')
+    let h5 = document.getElementsByTagName("h5");           //obtém as tags h5 da página
+    let numh5 = h5.length                                   //obtem quantidade de h5
+
+    let aux = -1;
+
+    for (let i = 0; i < numh5; i++) {
+        //se a tag é da class ParticipanteSelecionado (indica que esta selecionada)
+        if (h5[i].className == "ParticipanteSelecionado") {
+            h5[i].className = "Normal"  //troca para normal
+            aux = i;                    //muda valro da aux
+            break                       //sai da repetição
+        }
+    }
+    //se não há participante selecionado
+    if (aux == -1) {
+        alert("Primeiro selecione um participante para excluí-lo")
     }
 
+    //confirmação    
     let listaPessoas = localStorage.getItem("pessoasCadastradas").split(";");
-    let listaEmail = localStorage.getItem("emailCadastrado").split(";");
-    let listaDestino = localStorage.getItem("destinoCadastrado").split(";");
-    listaPessoas.shift();
-    let linhasPessoas = ""
+    let ok = confirm("Deseja excluir " + listaPessoas[aux] + "?")
+    if (ok == false) {
+        return;
+    } else {
 
-    //Excuir Pessoas
-    for (let i = 0; i < listaPessoas.length; i++) {
-        linhasPessoas += listaPessoas[i] + ";";
-        let pessoas = linhasPessoas.substr(0, linhasPessoas.length - 1);
-        localStorage.setItem("pessoasCadastradas", pessoas);
+        let listaPessoas = localStorage.getItem("pessoasCadastradas").split(";");
+        let listaEmail = localStorage.getItem("emailCadastrado").split(";");
+        let listaDestino = localStorage.getItem("destinoCadastrado").split(";");
+
+        //Excuir Pessoas
+        listaPessoas.splice(aux, 1);
+        localStorage.setItem("pessoasCadastradas", listaPessoas.join(';'));
+
+        //excluir email
+        listaEmail.splice(aux, 1);
+        localStorage.setItem("emailCadastrado", listaEmail.join(';'));
+
+        //excluir destino
+        listaDestino.splice(aux, 1);
+        localStorage.setItem("destinoCadastrado", listaDestino.join(';'));
+
+        if (listaPessoas.length == 0) {
+            localStorage.removeItem("pessoasCadastradas");
+            localStorage.removeItem("emailCadastrado");
+            localStorage.removeItem("destinoCadastrado");
+        }
     }
-
-    //excluir email
-    listaEmail.shift();
-    let linhasEmail = ""
-    for (let i = 0; i < listaEmail.length; i++) {
-        linhasEmail += listaEmail[i] + ";";
-        let email = linhasEmail.substr(0, linhasEmail.length - 1);
-        localStorage.setItem("emailCadastrado", email);
-    }
-
-    //excluir destino
-    listaDestino.shift();
-    let linhasDestino = ""
-    for (let i = 0; i < listaDestino.length; i++) {
-        linhasDestino += listaDestino[i] + ";";
-        let destino = linhasDestino.substr(0, linhasDestino.length - 1);
-        localStorage.setItem("destinoCadastrado", destino);
-    }
-
-
-    if (linhasPessoas.length == 0) {
-        localStorage.removeItem("pessoasCadastradas");
-        localStorage.removeItem("emailCadastrado");
-        localStorage.removeItem("destinoCadastrado");
-    }
-
+    location.reload();
     mostrarLista();
 }
 let btExcluir = document.querySelector("#btExcluir");
@@ -187,15 +228,13 @@ const limparLista = () => {
             localStorage.removeItem("emailCadastrado");
             localStorage.removeItem("destinoCadastrado");
         }
-
     }
 
-
+    location.reload();
     mostrarLista();
 }
 let btLimpar = document.getElementById("btLimpar");
 btLimpar.addEventListener("click", limparLista);
-
 
 
 //botão sortear
@@ -205,15 +244,31 @@ const sortear = () => {
     }
 
     let participantes = localStorage.getItem("pessoasCadastradas").split(";");
+    //sortear vencedor
+    let vencedor = participantes[Math.floor(Math.random() * participantes.length)]; //random do nome
 
 
+    //buscar o pais do vencedor
+    let cont = -1;
+    for (let i = 0; i < participantes.length; i++) {
+        cont++
+        if (vencedor == participantes[i]) {
+            break
+        }
+    }
+    let locais = localStorage.getItem("destinoCadastrado").split(";");
+    let localVencedor = "";
+    for (let i = 0; i < locais.length; i++) {
+        if (i == cont) {
+            localVencedor = locais[i]
+        }
+    }
 
-    participantes = participantes[Math.floor(Math.random() * participantes.length)]; //random do nome
-    let mensagem = "Parabéns " + participantes + " você é o(a) grande vencedor(a)!";
+    alert("Parabéns " + vencedor + "! Você ganou uma viagem para\n" +
+        "------------------------------------------\n" +
+        localVencedor);
 
-    alert(mensagem);
-
-    // console.log(participantes);
+    
 }
 let btSortear = document.querySelector('#btSortear');
 btSortear.addEventListener("click", sortear)
